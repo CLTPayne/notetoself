5c29119c854d0e1f1d479c3664c0c0f028c2d1be
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-07-28 15:02:53 +0100

    Add behaviour testing for App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -26,8 +26,61 @@ describe('<App />', () => {
 
     it('Renders a submit button', () => {
       expect(app.find('.btn').at(0).text()).toEqual('Submit')
-    })
+    });
 
+  });
+
+  describe('When creating a note', () => {
+    let testNote = 'test note';
+
+    beforeEach(() => {
+      app.find('FormControl').simulate('change', {
+        target: { value: testNote }
+      });
+    });
+
+    it('Updates the text in state', () => {
+      expect(app.state().text).toEqual(testNote)
+    });
+
+    describe('On submitting the new note', () => {
+
+      beforeEach(() => {
+        app.find('.btn').at(0).simulate('click')
+      });
+
+      afterEach(() => {
+        app.find('.btn').at(1).simulate('click')
+      });
+
+      it('Adds the new note to state', () => {
+        expect(app.state().notes[0].text).toEqual(testNote)
+      });
+
+      describe('And remounts the component', () => {
+        let app2;
+
+        beforeEach(() => {
+          app2 = mount(<App />)
+        });
+
+        it('Reads the stored note cookies', () => {
+          expect(app2.state().notes).toEqual([{ text: testNote }])
+        })
+      })
+
+      describe('On clicking the clear button', () => {
+
+        beforeEach(() => {
+          app.find('.btn').at(1).simulate('click')
+        });
+
+        it('Clears the notes in state', () => {
+          expect(app.state().notes).toEqual([])
+        })
+      })
+
+    })
   })
 
 });
